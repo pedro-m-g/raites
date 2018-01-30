0f154c09037754612e7dffeb9ba15b19cbc0b7d8
Pedro Martín del Campo González <pedro.martindelcampo.gonzalez@gmail.com>
2018-01-30 09:02:06 -0800

    Kickstart

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña debe ser de al menos 6 caracteres y debe confirmarse correctamente.',
+    'reset' => 'La contraseña ha sido restablecida!',
+    'sent' => 'Hemos enviado un link a tu correo electrónico para que puedas cambiar tu contraseña',
+    'token' => 'El link ha expirado.',
+    'user' => "No encontramos ningún registro con este correo. Es probable que aún no estés registrado.",
+
+];
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div id="app">
+        @include('partials.navbar')
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
